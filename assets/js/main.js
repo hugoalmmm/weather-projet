a894d7ef65b91f6b8ba09cccfddbf6083bfea75f
hugo <150128350+hugoalmmm@users.noreply.github.com>
2024-11-26 13:30:50 +0100

    /

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,9 @@ function useApi() {
       fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m", requestOptions)
         .then((response) => response.json())
         .then((result) => {
-
-            console.log(result.latitude)
+            console.log(result.hourly.temperature_2m)
+            document.querySelector('.temperature-of-city').textContent = `${result.elevation} ${result.current_units.temperature_2m}` 
+            document.querySelector('.max-temperature').textContent = Math.max(...result.hourly.temperature_2m)
 
 
         })
