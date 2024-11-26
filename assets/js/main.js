534e168c938ffd1a43f567ac94612ae71918c193
hugo <150128350+hugoalmmm@users.noreply.github.com>
2024-11-26 14:43:12 +0100

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,10 +7,19 @@ function useApi() {
       fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m", requestOptions)
         .then((response) => response.json())
         .then((result) => {
-            console.log(result.hourly.temperature_2m)
-            document.querySelector('.temperature-of-city').textContent = `${result.elevation} ${result.current_units.temperature_2m}` 
-            document.querySelector('.max-temperature').textContent = Math.max(...result.hourly.temperature_2m)
+            let humidityResult = 0 
+            result.hourly.relative_humidity_2m.forEach(element => humidityResult += element)
+            humidityResult = Math.round(humidityResult / result.hourly.relative_humidity_2m.length)
 
+            let windSpeed = 0
+            result.hourly.wind_speed_10m.forEach(element => windSpeed += element)
+            windSpeed = Math.round(windSpeed / result.hourly.wind_speed_10m.length)
+            document.querySelector('.temperature-of-city').textContent = `${result.elevation} ${result.current_units.temperature_2m}` 
+            document.querySelector('.max-temperature').textContent = Math.round(Math.max(...result.hourly.temperature_2m))
+            document.querySelector('.lowest-temperature').textContent = Math.min(...result.hourly.temperature_2m) 
+            document.querySelector('.feels-like').textContent = `${result.elevation} ${result.current_units.temperature_2m}`
+            document.querySelector('.humidity').textContent = `${humidityResult} ${result.hourly_units.relative_humidity_2m}`
+            document.querySelector('.ene-wind').textContent = `${windSpeed} ${result.hourly_units.wind_speed_10m}`
 
         })
         .catch((error) => console.error(error));
